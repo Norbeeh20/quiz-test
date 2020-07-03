732870e3889e5a4309519e75adb249d1ab619549
Norbeeh20 <zomborinorbi@hotmail.hu>
2020-07-04 00:50:36 +0200

    Updates

---
+++ precache-manifest.f93ae12b2274a6b288d84ef603c7089e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef30a7e9516c52db4065a6e4c7d13f64",
+    "url": "/quiz-test/index.html"
+  },
+  {
+    "revision": "55c676d12ba9d1c50376",
+    "url": "/quiz-test/static/css/main.1c58f974.chunk.css"
+  },
+  {
+    "revision": "c23d33c0f488a92ace02",
+    "url": "/quiz-test/static/js/2.97bbaf4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-test/static/js/2.97bbaf4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c676d12ba9d1c50376",
+    "url": "/quiz-test/static/js/main.85c6b77f.chunk.js"
+  },
+  {
+    "revision": "5fa71b905623c5ef72de",
+    "url": "/quiz-test/static/js/runtime-main.c52cc43b.js"
+  },
+  {
+    "revision": "04c40d93fbd559c8f7e395b0499e8250",
+    "url": "/quiz-test/static/media/background.04c40d93.png"
+  }
+]);
